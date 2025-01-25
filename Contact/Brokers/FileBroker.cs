8f9a9f37f230f9bd5b6ec4473526991597994192
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-01-25 05:44:47 +0500

    CREATE: Brokers:FileBroker.cs finish

--- Contact/Brokers/FileBroker.cs
+++ Contact/Brokers/FileBroker.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Contact.Brokers
 {
     internal class FileBroker
     {
+        //ContactsInfo sinfidan obyect oldim proplardan foydalanish uchun
+        ContactsInfo Contacts= new ContactsInfo();
+
+        //qiymat faqat constructor orqali o'rnatiladi va faqat o'qiladi bu fayl joylashuvi;
+        private readonly string _filePath;
+
+        //Constructor
+        public FileBroker(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        //contactni o'qish uchun
+        public List<ContactsInfo> ReadContacts()
+        {
+            if (!File.Exists(_filePath))
+                return new List<ContactsInfo>();
+            var jsonData = File.ReadAllText(_filePath);
+            return JsonConvert.DeserializeObject<List<ContactsInfo>>(jsonData) ?? new List<ContactsInfo>();
+
+
+        }
+        public void WriteContacts(List<ContactsInfo> contacts)
+        {
+            var jsonData = JsonConvert.SerializeObject(contacts, Formatting.Indented);
+            File.WriteAllText(_filePath, jsonData);
+        }
     }
+
 }
---
+++ Contact/ContactsInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact
+{
+    internal class ContactsInfo
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
