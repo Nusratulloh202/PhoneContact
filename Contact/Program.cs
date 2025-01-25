c0c5e2c4ebc81321f0a51d5bbb6a0bf6d31a1ab5
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-01-25 05:06:04 +0500

    INFRA:Logging Broker and Program Swich case finish

--- Contact/Program.cs
+++ Contact/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Phone Contact project!");
+﻿using Contact.Brokers;
+using Contact.Service;
+
+var phoneBookService = new PhoneBookService();
+while (true)
+{
+    Console.WriteLine("\nTelefon kitobi dasturi:");
+    Console.WriteLine("1. Yangi kontakt qo'shish");
+    Console.WriteLine("2. Kontaktni tahrirlash");
+    Console.WriteLine("3. Kontaktni o'chirish");
+    Console.WriteLine("4. Barcha kontaktlarni ko'rish");
+    Console.WriteLine("5. Kontaktni nomi bo'yicha qidirish");
+    Console.WriteLine("6. Chiqish");
+    Console.Write("Tanlovingizni kiriting: ");
+    try
+    {
+        int choice = int.Parse(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                phoneBookService.AddContact();
+                break;
+            case 2:
+                phoneBookService.EditContact();
+                break;
+            case 3:
+                phoneBookService.DeleteContact();
+                break;
+            case 4:
+                phoneBookService.ViewAllContact();
+                break;
+            case 5:
+                phoneBookService.SearchContact();
+                break;
+            case 6:
+                Console.WriteLine("Chiqish...");
+                return;
+            default:
+                Console.Write("Noto'g'ri tanlov. Qayta kiriting:");
+                break;
+        }
+    }
+    catch(Exception exp)
+    {
+        LoggingBroker.LogError(exp.Message);
+    }
+}
